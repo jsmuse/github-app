a473a68787f8a691172cd0cd5dfd4c9cb9fbc141
Julia Kravchenko <yuliakatmuse@gmail.com>
2020-06-12 18:21:06 +0300

    add user cards in app and add users request

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useState, useEffect } from "react";
+import { Pagination } from "@material-ui/lab";
+import { CircularProgress, Grid } from "@material-ui/core";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import UserCard from "./components/UserCard/UserCard";
+import styles from "./App.module.scss";
+
+export default function App() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+  const [pagination, setPagination] = useState(1);
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users?since=${pagination}0`)
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  }, [pagination]);
+
+  const handleChange = (event, page) => {
+    setPagination(page);
+  };
+
+  console.log(items);
+  if (error) {
+    return <div>Ошибка: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <CircularProgress disableShrink />;
+  } else {
+    return (
+      <div className={styles.app}>
+        <Grid container justify="center" alignItems="center">
+          <Grid container item xs={10}>
+            {items.map(({ login, avatar_url, html_url }) => (
+              <UserCard
+                key={login}
+                name={login}
+                imageLink={avatar_url}
+                link={html_url}
+              />
+            ))}
+          </Grid>
+
+          <Pagination
+            variant="outlined"
+            size="large"
+            count={4}
+            page={pagination}
+            onChange={handleChange}
+          />
+        </Grid>
+      </div>
+    );
+  }
 }
-
-export default App;
