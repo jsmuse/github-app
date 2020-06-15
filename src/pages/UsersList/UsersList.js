9812c15490baac5e1e536600c22db75f6f0b739c
Julia Kravchenko <yuliakatmuse@gmail.com>
2020-06-15 09:37:10 +0300

    extract UserProfile to pages

---
+++ src/pages/UsersList/UsersList.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from "react";
+import { Pagination } from "@material-ui/lab";
+import { CircularProgress, Grid } from "@material-ui/core";
+
+import UserCard from "../../components/UserCard/UserCard";
+//import styles from "./UsersList.module.scss";
+
+export default function UsersList() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+  const [pagination, setPagination] = useState(1);
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users?since=${pagination}00`)
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  }, [pagination]);
+
+  const handleChange = (event, page) => {
+    setPagination(page);
+  };
+
+  if (error) {
+    return <div>Ошибка: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <CircularProgress disableShrink />;
+  } else {
+    return (
+      <>
+        <Grid container item xs={10}>
+          {items.map(({ login, avatar_url, html_url }) => (
+            <UserCard
+              key={login}
+              name={login}
+              imageLink={avatar_url}
+              link={html_url}
+            />
+          ))}
+        </Grid>
+
+        <Pagination
+          variant="outlined"
+          size="large"
+          count={4}
+          page={pagination}
+          onChange={handleChange}
+        />
+      </>
+    );
+  }
+}
